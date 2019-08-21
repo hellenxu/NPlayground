0ca605682a90e2c6f068ba0f3d4a5bfb9b082561
hellenxu <heavensxu@gmail.com>
2019-08-20 21:46:00 -0400

    add other two methods of creating and updating files.

--- basics/fs/fsExample.js
+++ basics/fs/fsExample.js
@@ -1,7 +1,19 @@
-const fs = require('fs');
+const fs = require("fs");
 
-// similar function: appendFile()
-fs.writeFile('../data/file001.txt', 'Hello~ Creating new files', function(err) {
+// Create or update files -- writeFile [replace the specific file and content if it exists]
+fs.writeFile("../data/file001.txt", "Hello~ Creating new files", function(err) {
   if (err) throw err;
-  console.log('Files saved...');
+  console.log("Files saved...");
+});
+
+// Create or update files -- appendFile [append contents to a file]
+fs.appendFile("../data/file002.txt", "Hello~ appendFile()", function(err) {
+  if (err) throw err;
+  console.log(`Files saved(appendFile)...`);
+});
+
+// Create files -- open [create a new empty file]
+fs.open("../data/file003.txt", "w", function(err) {
+  if (err) throw err;
+  console.log(`Files created(open)...`);
 });
