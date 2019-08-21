8afb26a1ac71d63f4bd9723aa545ca684c4eca96
hellenxu <heavensxu@gmail.com>
2019-08-20 20:58:09 -0400

    add fs read files example.

--- basics/http/httpSample.js
+++ basics/http/httpSample.js
@@ -1,8 +1,19 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
+// example: parsing query string
+// http.createServer(function(req, res) {
+//   const query = url.parse(req.url, true).query;
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end(`${query.language} ${query.location}`);
+// }).listen(4008);
+
+// example: read files
 http.createServer(function(req, res) {
-  const query = url.parse(req.url, true).query;
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`${query.language} ${query.location}`);
+  fs.readFile("../data/test.html", function(err, data) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    res.end();
+  });
 }).listen(4008);
