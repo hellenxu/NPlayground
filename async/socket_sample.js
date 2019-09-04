6e15022b21e7ceaa4623925ad66048d5c4d00568
hellenxu <heavensxu@gmail.com>
2019-09-03 23:07:50 -0400

    added a broken channel server.

--- async/socket_sample.js
+++ async/socket_sample.js
@@ -7,8 +7,37 @@ const net = require('net');
 // }).listen(4008);
 
 //socket#once only responds to a single event
-net.createServer(socket => {
-  socket.once('data', (data) => {
-    socket.write(data);
+// net.createServer(socket => {
+//   socket.once('data', (data) => {
+//     socket.write(data);
+//   });
+// }).listen(4008);
+
+const events = require('events');
+
+const channel = new events.EventEmitter();
+channel.clients = {};
+channel.subscriptions = {};
+
+channel.on('join', (id, client) => {
+  this.clients[id] = client;
+  this.subscriptions[id] = (senderId, message) => {
+    if (id !== senderId) {
+      this.clients[id].write(message);
+    }
+  };
+  this.on('broadcast', this.subscriptions[id])
+});
+
+const server = net.createServer((client) => {
+  const id = `${client.remoteAddress}:${client.remotePort}`;
+  client.on('connect', () => {
+    channel.emit('join', id, client);
   });
-}).listen(4008);
+  client.on('data', (data) => {
+    data = data.toString();
+    channel.emit('broadcast', id, data);
+  });
+});
+
+server.listen(4008);
