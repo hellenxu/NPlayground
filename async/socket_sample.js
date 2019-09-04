914965e0807e188ae09797a7eb54b5347213125c
hellenxu <heavensxu@gmail.com>
2019-09-03 22:51:37 -0400

    add socket#once example.

--- async/socket_sample.js
+++ async/socket_sample.js
@@ -1,7 +1,14 @@
 const net = require('net');
 
+// net.createServer(socket => {
+//   socket.on('data', (data) => {
+//     socket.write(data);
+//   });
+// }).listen(4008);
+
+//socket#once only responds to a single event
 net.createServer(socket => {
-  socket.on('data', (data) => {
+  socket.once('data', (data) => {
     socket.write(data);
   });
 }).listen(4008);
