c789b915cc70c90c059c021cd0cf3f82d61e2134
hellenxu <heavensxu@gmail.com>
2019-09-10 22:29:15 -0400

    Added: a subclass of EventEmitter.

---
+++ event/FileWatcher.js
@@ -0,0 +1,33 @@
+const events = require('events');
+const fs = require('fs');
+// const util = require('util');
+
+// es6 uses extends to inherit another object's behaviors.
+class FileWatcher extends events.EventEmitter{
+  constructor(watchDir, processedDir) {
+    super();
+    this.watchDir = watchDir;
+    this.processedDir = processedDir;
+  }
+
+  watch() {
+    const watcher = this;
+    fs.readdir(this.watchDir, ((err, files) => {
+      if (err) throw err;
+      files.forEach((file) => {
+        watcher.emit('process', file);
+      });
+    }));
+  }
+
+  start() {
+    const watcher = this;
+    fs.watchFile(this.watchDir, () => {
+      watcher.watch();
+    });
+  }
+
+}
+
+// old way to inherit another object's behaviors
+// util.inherits(FileWatcher, events.EventEmitter);
