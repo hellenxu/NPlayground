b099f1c4d87c0369cc542becfafb8de77d5b15de
hellenxu <heavensxu@gmail.com>
2019-11-20 21:20:10 -0500

    add lodash orderby and groupby examples.

--- basics/Sample.js
+++ basics/Sample.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 // scope example
 const change = function(a) {
   a = 100;
@@ -39,6 +41,18 @@ exports.method:  { name: 'module.exports: to iterate module paths',
   method: [Function: method] }
 exports.exports:  undefined
  */
+
+const groups = [
+  { id: "12345", groupId: 4, companyId: 5, name: "test1" },
+  { id: "12567", groupId: 4, companyId: 5, name: "test4" },
+  { id: "1095", groupId: 33, companyId: 34, name: "test5" },
+  { id: "1250", groupId: 33, companyId: 5, name: "test6" }
+];
+
+const groupResult = _.orderBy(_.groupBy(groups, 'groupId'), 'companyId');
+_.forEach(groupResult, (item) => {
+  console.log(`xxl-item: ${JSON.stringify(item)}`)
+});
 
 class sample {
     constructor(transformer) {
