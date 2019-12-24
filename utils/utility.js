2df43456442a5e529720322ca40478bb9fc6c2be
hellenxu <heavensxu@gmail.com>
2019-12-24 10:09:40 -0500

    add utility method: merge.

---
+++ utils/utility.js
@@ -0,0 +1,11 @@
+const _ = require('lodash');
+
+const merge = (output, ...input) => {
+  return _.mergeWith(output, ...input, (outValue, inValue) => {
+    return _.isBuffer(inValue) ? inValue : undefined
+  })
+};
+
+module.exports = {
+  merge
+};
