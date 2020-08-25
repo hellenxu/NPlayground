842dc36e48388bb27ccb15f91d92406fcfa4d353
hellenxu <heavensxu@gmail.com>
2020-08-24 21:16:25 -0400

    update: add output logs.

--- es6/Generator.js
+++ es6/Generator.js
@@ -10,3 +10,8 @@ console.log('xxl-1st:', fooIterator.next());
 console.log('xxl-2nd:', fooIterator.next());
 console.log('xxl-3rd:', fooIterator.next());
 console.log('xxl-4th:', fooIterator.next());
+// output above:
+// xxl-1st: { value: 'hello', done: false }
+// xxl-2nd: { value: 'generator', done: false }
+// xxl-3rd: { value: 'finished', done: true }
+// xxl-4th: { value: undefined, done: true }
