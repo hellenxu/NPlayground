be36cd8e797b2a998d028ca02f711b041e294a73
hellenxu <heavensxu@gmail.com>
2020-09-05 23:47:09 -0400

    add: hoisting example.

--- basics/ReviewBasics.js
+++ basics/ReviewBasics.js
@@ -7,3 +7,15 @@ function func(arg) {
 }
 
 func(18)
+
+var tmp = new Date()
+function f() {
+  console.log(`tmp: ${tmp}`)
+  if (false) {
+    var tmp = 'hello world'
+    console.log(`new tmp: ${tmp}`)
+  }
+}
+f()
+// output:
+// tmp: undefined
