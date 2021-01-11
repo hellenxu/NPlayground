b319e2d36aac4156973ed04f71ca19dd1b2f91ce
hellenxu <heavensxu@gmail.com>
2021-01-11 10:57:54 -0500

    add: several ways to show the same char.

--- basics/ReviewBasics.js
+++ basics/ReviewBasics.js
@@ -130,3 +130,9 @@ function mmove({x, y} = { x: 0, y: 0 }) {
 }
 console.log(`xxl-func-des: ${mmove()}`)
 console.log(`xxl-func-des1: ${mmove({x: 10, y: 20})}`)
+
+console.log(`${'\z' === 'z'}`)
+console.log(`${'\172' === 'z'}`)
+console.log(`${'\x7A' === 'z'}`)
+console.log(`${'\u007A' === 'z'}`)
+console.log(`${"\u{7A}" === 'z'}`)
